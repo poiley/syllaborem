6cf980895533d5cde4b272da49f5ecb01c41f955
poiley <bpoile@eecs.wsu.edu>
2021-02-23 20:07:12 -0800

    Added pdfPlumber support

--- apps/reader/core.py
+++ apps/reader/core.py
@@ -1,4 +1,4 @@
-from . import FILETYPES, textract, os
+from . import FILETYPES, pdfplumber, textract, os
 
 class Reader:
     def __init__(self):
@@ -6,7 +6,7 @@ class Reader:
 
     def read_file(self, file_dir):
         # Check if the directory supplied is valid
-        if not os.path.isdir(file_dir):
+        if not os.path.isfile(file_dir):
             return 'Invalid file directory'
 
         # Use python's built in library for handling paths as strings
@@ -16,4 +16,14 @@ class Reader:
         # Ensure that the file extension is within our list of pre-approved types
         if file_extension in FILETYPES:
             if file_extension == 'pdf':
+                file_contents = ''
+                file = pdfplumber.open(file_dir)
+
+                for page in file.pages:
+                    for character in page.chars:
+                        file_contents += character['text']
+                    file_contents += '\n'
+
+                return file_contents
+            elif file_extension == 'txt':
                 return textract.process(file_dir)
