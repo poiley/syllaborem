af8702ef15da911f3441ce2fb6199b38bc865c75
poiley <benjpoile@gmail.com>
2021-02-24 16:59:46 -0800

    updated tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
-from apps.user.models import User
+from apps.user.models import User as UserClass
+
+
+# import pdb;
+# pdb.set_trace()
 
 class UserTestCase(TestCase):
     def setUp(self):
-        self.user1 = User(username="chris_falken", email="cfalken@gmail.com")
-        self.user2 = User(username="keef_m00n", email="whoareu@gmail.com")
+        self.user1 = UserClass.objects.create(username='chris_falken')
+        self.user2 = UserClass.objects.create(username='keef_m00n')
+
+    def tearDown(self):
+        self.user1.delete()
+        self.user2.delete()
 
     def test_str(self):
         '''Test the __str__ function for the User model.'''
         self.assertEqual(self.user1.__str__(), 'User {username}'.format(username=self.user1.username))
         self.assertEqual(self.user2.__str__(), 'User {username}'.format(username=self.user2.username))
+
