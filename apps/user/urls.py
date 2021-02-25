54a8f04b1cb8b180a184c06beacf753cef95ef58
poiley <benjpoile@gmail.com>
2021-02-24 16:46:15 -0800

    changed everything... again. last time. i promise. should close #52

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from django.conf import settings
 from apps.user import views
+from apps.user.views import dashboard, register
 from django.conf.urls.static import static
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', dashboard, name='dashboard'),
+    url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^register/", register, name="register"),
 ]
