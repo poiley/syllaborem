b7639273b42094aba6d0c5cd78e8d41f630aa63f
poiley <benjpoile@gmail.com>
2021-02-24 17:03:51 -0800

    updated tests

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+
     def __str__(self):
         return 'User {username}'.format(username=self.username)
